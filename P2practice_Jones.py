e894937bfee93377511266c556e7bcda8a1ae2a6
jonesm1663 <44269439+jonesm1663@users.noreply.github.com>
2018-11-08 11:55:50 -0500

    Add files via upload
    First python github submisson.

---
+++ P2practice_Jones.py
@@ -0,0 +1,27 @@
+#cti110
+#P2practice_jones
+#Michael Jones
+#11/8/18
+
+#ask user their age
+#tell them how old they will be next year.
+
+#get input
+age=int(input("enter number:"))
+#calculate the answer
+answer=age+1
+#print output
+print("your resultis", answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
